6c158c0cb7413a285acff066f35cc1d22d0a2566
Shawon Ashraf <shawon13@live.com>
2015-04-02 23:05:18 +0600

    Added derp!!!

--- Lightoj/LightOJ 1148/LightOJ 1148/main.cpp
+++ Lightoj/LightOJ 1148/LightOJ 1148/main.cpp
@@ -1,15 +1,3 @@
-//
-//  main.cpp
-//  LightOJ 1148
-//
-//  Created by Shawon Ashraf on 4/2/15.
-//  Copyright (c) 2015 Shawon Ashraf. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
