1fce6c45c3c63a95b4306f7652da0d49bcd621e7
Shawon Ashraf <shawon13@live.com>
2015-04-02 23:04:46 +0600

    Added 1148

---
+++ Lightoj/LightOJ 1148/LightOJ 1148/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LightOJ 1148
+//
+//  Created by Shawon Ashraf on 4/2/15.
+//  Copyright (c) 2015 Shawon Ashraf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
