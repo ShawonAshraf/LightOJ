1ec47be97b30895211e277429a67f4476f03cfe3
Shawon Ashraf <shawon@shawon-s-macbook-pro.local>
2015-05-26 19:35:17 +0600

    Added 1179

---
+++ Lightoj/LightOJ 1179/LightOJ 1179/main.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int numberOfSurvivors(vector<int> &v, int &index);
+
+int main() {
+    int testCases;
+    
+    cin >> testCases;
+    
+    for (int i = 1; i <= testCases; i++) {
+        int n, k, index;
+        
+        cin >> n >> k;
+        
+        vector<int> v(n, 1);
+        
+        int x = 0;
+        while (numberOfSurvivors(v, index) != 1) {
+            x += (k - 1);
+            //v.erase(v.begin() + x);
+            v.at(x) = 0;
+        }
+        
+        cout << "Index of the last man standing : " << index << endl;
+    }
+    
+    return 0;
+}
+
+int numberOfSurvivors(vector<int> &v, int &index) {
+    int aliveHeadCount = 0;
+    int i = 0;
+    
+    int s = (int) v.size();
+    for (i = 0; i <= s; i++) {
+        if (v[i]) {
+            aliveHeadCount++;
+            index = i;
+        }
+    }
+    
+    return aliveHeadCount;
+}
